cbdae724a282a16ef8e73a08b52a96df813e0944
Gordon Woodhull <gordon@woodhull.com>
2023-04-24 10:29:06 -0400

    fix tests for consistent directions

--- src/lib/puzzle/grids/cubegrid.test.js
+++ src/lib/puzzle/grids/cubegrid.test.js
@@ -4,51 +4,51 @@ import { CubeGrid } from './cubegrid';
 describe('Test making a cell empty', () => {
 	const grid = new CubeGrid(3, 3, false);
 	grid.makeEmpty(14);
-
-	it('Has no neighbors on border', () => {
-		const { neighbour, empty } = grid.find_neighbour(4, 4);
-		expect(neighbour).toBe(-1);
-		expect(empty).toBe(true);
-	});
-
-	it('Has no neighbors on border', () => {
-		const { neighbour, empty } = grid.find_neighbour(4, 8);
-		expect(neighbour).toBe(-1);
-		expect(empty).toBe(true);
-	});
-
-	it('Has no neighbors on border', () => {
-		const { neighbour, empty } = grid.find_neighbour(11, 4);
-		expect(neighbour).toBe(-1);
-		expect(empty).toBe(true);
-	});
-
-	it('Has no neighbors on border', () => {
-		const { neighbour, empty } = grid.find_neighbour(24, 8);
-		expect(neighbour).toBe(-1);
-		expect(empty).toBe(true);
-	});
 	
 	it('Reports an empty neighbour', () => {
-		const { neighbour, empty } = grid.find_neighbour(9, 1);
+		const { neighbour, empty } = grid.find_neighbour(9, 8);
 		expect(neighbour).toBe(14);
 		expect(empty).toBe(true);
 	});
 
 	it('Reports another empty neighbour', () => {
-		const { neighbour, empty } = grid.find_neighbour(12, 4);
+		const { neighbour, empty } = grid.find_neighbour(12, 2);
 		expect(neighbour).toBe(14);
 		expect(empty).toBe(true);
 	});
 
-    it('Reports an non-empty neighbour', () => {
-		const { neighbour, empty } = grid.find_neighbour(9, 8);
+    it('Reports a non-empty neighbour', () => {
+		const { neighbour, empty } = grid.find_neighbour(9, 4);
 		expect(neighbour).toBe(22);
 		expect(empty).toBe(false);
 	});
 
-	it('Reports an non-neighbour when going outside the grid', () => {
-		const { neighbour, empty } = grid.find_neighbour(18, 8);
+	it('Reports a non-neighbour when going outside the grid', () => {
+		const { neighbour, empty } = grid.find_neighbour(18, 4);
+		expect(neighbour).toBe(-1);
+		expect(empty).toBe(true);
+	});
+
+	it('Reports a non-neighbour when going outside the grid', () => {
+		const { neighbour, empty } = grid.find_neighbour(4, 4);
+		expect(neighbour).toBe(-1);
+		expect(empty).toBe(true);
+	});
+
+	it('Reports a non-neighbour when going outside the grid', () => {
+		const { neighbour, empty } = grid.find_neighbour(4, 8);
+		expect(neighbour).toBe(-1);
+		expect(empty).toBe(true);
+	});
+
+	it('Reports a non-neighbour when going outside the grid', () => {
+		const { neighbour, empty } = grid.find_neighbour(11, 4);
+		expect(neighbour).toBe(-1);
+		expect(empty).toBe(true);
+	});
+
+	it('Reports a non-neighbour when going outside the grid', () => {
+		const { neighbour, empty } = grid.find_neighbour(24, 8);
 		expect(neighbour).toBe(-1);
 		expect(empty).toBe(true);
 	});
